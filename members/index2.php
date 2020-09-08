59838dca6edf2991eba64c80fc4b131f4ddd17ee
Hsieh-cy <lmjyy5v8331@gmail.com>
2020-09-09 03:38:12 +0800

    update upload picture

---
+++ members/index2.php
@@ -0,0 +1,19 @@
+<?php require __DIR__ . '/../config/_connect_db.php'; ?>
+<?php require __DIR__ . '/../views/views/_html_head.php'; ?>
+<?php include __DIR__ . '/../views/_navbar.php'; ?>
+
+
+<body>
+    <div style="text-align: center; padding: 10px">
+        Hello World!
+    </div>
+
+    <!-- <div style="text-align: center; padding: 10px">
+        <?= WEB_ROOT ?>
+    </div> -->
+
+</body>
+
+
+<?php require __DIR__ . '/../views/views/_scripts.php'; ?>
+<?php include __DIR__ . '/../views/views/_html_foot.php'; ?>
